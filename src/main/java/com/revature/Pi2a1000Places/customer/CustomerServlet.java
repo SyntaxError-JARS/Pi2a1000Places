3b87dd0c76f5cf230d3906799eeecf4a992cec60
mmoord <92188851+mmoord@users.noreply.github.com>
2022-06-03 11:36:01 -0400

    Update CustomerServlet.java

--- src/main/java/com/revature/Pi2a1000Places/customer/CustomerServlet.java
+++ src/main/java/com/revature/Pi2a1000Places/customer/CustomerServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CustomerServlet extends HttpServlet {
+public class CustomerServlet extends HttpServlet implements Authable {
 
     private final CustomerServices customerServices;
     private final ObjectMapper mapper;
