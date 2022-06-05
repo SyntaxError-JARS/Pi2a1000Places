8f23cf85136b665fa436bb87f86010036ae786e8
MelroseDigital <Tenyce.Melrose@gmail.com>
2022-06-05 08:04:45 -0700

    "Post-Azure Updates"

--- src/main/java/com/revature/Pi2a1000Places/util/HibernateUtil.java
+++ src/main/java/com/revature/Pi2a1000Places/util/HibernateUtil.java
@@ -1,12 +1,14 @@
 package com.revature.Pi2a1000Places.util;
 
 //import com.revature.Pi2a1000Places.customer.Customer;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
+import javax.imageio.spi.ServiceRegistry;
+import javax.security.auth.login.AppConfigurationEntry;
+import javax.security.auth.login.Configuration;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -16,8 +18,13 @@ public class HibernateUtil {
     private static Session session;
 
     public static Session getSession() throws IOException {
-        if(sessionFactory == null) {
-            Configuration configuration = new Configuration();
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration() {
+                @Override
+                public AppConfigurationEntry[] getAppConfigurationEntry(String name) {
+                    return new AppConfigurationEntry[0];
+                }
+            };
             Properties props = new Properties();
 
             ClassLoader loader = Thread.currentThread().getContextClassLoader();
@@ -25,14 +32,13 @@ public class HibernateUtil {
 
             configuration.setProperties(props);
 
-        //    configuration.addAnnotatedClass(Customer.class);
+            //    configuration.addAnnotatedClass(Customer.class);
 
 
             //configuration.addAnnotatedClass(CreditCard.class);
             //configuration.addAnnotatedClass(Menu.class);
             //configuration.addAnnotatedClass(Order.class);
 
-            
 
             // ServiceRegistry
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
@@ -40,11 +46,9 @@ public class HibernateUtil {
 
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 
-
-
         }
 
-        if(session == null) {
+        if (session == null) {
             session = sessionFactory.openSession();
         }
 
@@ -56,7 +60,23 @@ public class HibernateUtil {
         session = null;
 
     }
-}
+
+                //See Lecture 5/27/2022 @21 minutes (added 6/4/22)- Azure App Configurations
+                //Deploy via terminal (all 10 lines), then sign into acct
+
+            String url = system.getenv(SQLAZURECONNSTR Pito1000DB);
+            String username = system.getenv(name: "Dbuser")
+            String password = system.getenv(password: "Dbpassword")
+
+            configuration.setCurrentTenantIdentifierResolver("hibernate.connection.url", url);
+            configuration.setCurrentTenantIdentifierResolver("hibernate.connection.username", username);
+            configuration.setCurrentTenantIdentifierResolver("hibernate.connection.password", password);
+            configuration.setCurrentTenantIdentifierResolver("hibernate.connection.hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
+            configuration.setCurrentTenantIdentifierResolver("hibernate.connection.hibernate.connection.driver_class", "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            configuration.setCurrentTenantIdentifierResolver("hibernate.connection.hibernate.show_sql", "true");
+            configuration.setCurrentTenantIdentifierResolver("hibernate.connection.hibernate.hbm2ddl.auto", "update");
+
+
 
 //        hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect
 //        hibernate.connection.driver_class=org.postgresql.Driver
